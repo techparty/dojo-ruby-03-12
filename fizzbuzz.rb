bf9c355b1b70f6b98d5aa2ec9e2118762dfa6d12
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-03-10 20:17:53 -0300

    refactored

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,7 @@
 class Fizzbuzz
-	def fizzbuzz(num)
-		fizzbuzz = "#{'fizz' if num % 3 == 0}#{'buzz' if num % 5 == 0}"
-		if fizzbuzz != ""
-			return fizzbuzz
-		else
-			return num
-		end
-	end
+  def fizzbuzz(num)
+    fizzbuzz = "#{'fizz' if num % 3 == 0}#{'buzz' if num % 5 == 0}"
+    return fizzbuzz unless fizzbuzz.empty?
+    num
+  end
 end
